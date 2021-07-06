505a46988da42e6f98941d90db42b2c3ca0d74fa
RehamMobarak <reham.a.mobarak@gmail.com>
2021-07-06 02:32:03 +0200

    add:edit view

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,108 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Edit Listing
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="md:grid md:grid-cols-1 md:gap-6">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form action="/listings/{{$listing->id}}/edit" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="shadow sm:rounded-md sm:overflow-hidden">
+
+                        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                            <div class="grid grid-cols-3 gap-6">
+
+                                <div>
+                                    <label for="name" class="block text-sm font-medium text-gray-700">
+                                        Name
+                                    </label>
+                                    <div class="mt-1">
+                                        <input type="text" name="name" id="name"
+                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                            placeholder="listing name" value="{{$listing->Name}}">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="address" class="block text-sm font-medium text-gray-700">
+                                        Address
+                                    </label>
+                                    <div class="mt-1">
+                                        <input type="text" name="address" id="address"
+                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                            placeholder="listing address" value="{{$listing->Address}}">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="email" class="block text-sm font-medium text-gray-700">
+                                        Email
+                                    </label>
+                                    <div class="mt-1">
+                                        <input type="text" name="email" id="email"
+                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                            placeholder="listing email" value="{{$listing->Email}}">
+                                    </div>
+                                </div>
+
+                                {{-- <div class="col-span-3 sm:col-span-2"> --}}
+                                <div>
+                                    <label for="company-website" class="block text-sm font-medium text-gray-700">
+                                        Website
+                                    </label>
+                                    <div class="mt-1 flex rounded-md shadow-sm">
+                                        <span
+                                            class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                                            http://
+                                        </span>
+                                        <input type="text" name="website" id="website"
+                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                            placeholder="www.example.com" value="{{$listing->Website}}">
+                                    </div>
+                                </div>
+
+
+                                <div>
+                                    <label for="phone" class="block text-sm font-medium text-gray-700">
+                                        Phone
+                                    </label>
+                                    <div class="mt-1">
+                                        <input type="text" name="phone" id="phone"
+                                            class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                            placeholder="listing phone" value="{{$listing->Phone}}">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label for="bio" class="block text-sm font-medium text-gray-700">
+                                        Bio
+                                    </label>
+                                    <div class="mt-1">
+                                        <textarea id="bio" name="bio" rows="3"
+                                            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                                            placeholder="listing bio">{{$listing->Bio}}</textarea>
+                                    </div>
+                                    <p class="mt-2 text-sm text-gray-500">
+                                        Brief description for your Listing. URLs are hyperlinked.
+                                    </p>
+                                </div>
+
+
+
+                            </div>
+                            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                <button type="submit"
+                                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Edit
+                                </button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
